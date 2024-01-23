bd5054c9809477f45a76cd45061b6b2272abaa6c
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2024-01-23 23:08:01 +0300

    Add project files.

---
+++ WebUygulamasıProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebUygulamasıProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
